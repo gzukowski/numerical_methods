499c00aefa38a114a119665acd6c2f9127f96076
gzukowski <85632612+gzukowski@users.noreply.github.com>
2024-03-11 20:51:15 +0100

    Finish

--- macd/main.py
+++ macd/main.py
@@ -1,42 +1,10 @@
 from data import read_csv
-from methods import macd, signal
-import matplotlib.pyplot as plt
-import pandas as pd
+from methods import macd, signal, analyze_macd, display_chart, simulate_investment, simulate_investment_rsi
 
 
 
 FILENAME = "xauusd_d.csv"
-INITIAL_BALANCE = 10000
-BUY = 1
-SELL = 0
-
-
-def simulate_investment(intersection_points: list, dataset_display: pd.DataFrame) -> float:
-      balance = INITIAL_BALANCE
-      coins = 0
-
-      for point in intersection_points:
-            index, _, state = point
-            price = dataset_display["close"][index]
-
-            if state == BUY:
-                  coins_to_buy = balance / price
-                  balance -= coins_to_buy * price
-                  coins += coins_to_buy
-                  print(f"Buying {coins_to_buy:.8f} coins at ${price:.2f} each. Balance: ${balance:.2f}")
-            elif state == SELL:
-                  balance += coins * price
-                  print(f"Selling {coins:.8f} coins at ${price:.2f} each. Balance: ${balance:.2f}")
-                  coins = 0
-
-      if coins > 0:
-            balance += coins * dataset_display["close"].iloc[-1]
-            print(f"Selling remaining {coins:.8f} coins at ${dataset_display['close'].iloc[-1]:.2f} each. Balance: ${balance:.2f}")
-
-      return balance
-
-
-
+LABEL = "BTC"
 
 
 
@@ -45,138 +13,38 @@ def main() -> None:
       
       dataset = read_csv(filename=FILENAME)
 
-      
-
       dataset_display = dataset.tail(1000)
-      print(dataset_display)
 
       macd_values = macd(dataset["close"])
       signal_values = signal(macd_values)
 
 
-      plt.figure(figsize=(10, 6))
-      plt.plot(dataset_display.index, macd_values, label='MACD', color='blue')
-      plt.plot(dataset_display.index, signal_values, label='Signal Line', color='red')
-
-
-      buy_points = []
-      sell_points = []
-      intersection_points = []
-      for i in range(1, len(macd_values)):
-            if macd_values[i] > signal_values[i] and macd_values[i - 1] <= signal_values[i - 1]:
-                  buy_points.append((dataset_display.index[i], dataset_display["close"].iloc[i]))
-                  intersection_points.append((dataset_display.index[i], macd_values[i], BUY))
-            elif macd_values[i] < signal_values[i] and macd_values[i - 1] >= signal_values[i - 1]:
-                  sell_points.append((dataset_display.index[i], dataset_display["close"].iloc[i]))
-                  intersection_points.append((dataset_display.index[i], macd_values[i], SELL))
+      buy_points, sell_points, intersection_points = analyze_macd(macd_values, signal_values, dataset_display)
 
       final_balance = simulate_investment(intersection_points, dataset_display)
       print("Final balance after simulation:", final_balance)
+      print("\n \n")
 
-      for point in buy_points:
-            plt.scatter(point[0], point[1], color='green', marker='^', s=50, zorder=3)
-
-      for point in sell_points:
-            plt.scatter(point[0], point[1], color='black', marker='v', s=50, zorder=3)
-
-      for point, value, _ in intersection_points:
-            plt.scatter(point, value, color='orange', marker='o', s=50, zorder=3)
+      final_balance = simulate_investment_rsi(intersection_points, dataset_display)
+      print("Final balance after simulation:", final_balance)
 
 
+      
+      # wykres 1000 dni
+      display_chart(macd_values, signal_values, dataset_display, buy_points, sell_points, intersection_points, LABEL)
+      
+      # wykres 500 dni
+
+      buy_points, sell_points, intersection_points = analyze_macd(macd_values, signal_values, dataset_display, 2)
+
+      display_chart(macd_values, signal_values, dataset_display, buy_points, sell_points, intersection_points, LABEL, 500)
 
 
-      plt.plot(dataset_display.index, dataset_display["close"], label='AU', color='yellow', zorder=2)
-      plt.title('MACD and Signal Line')
-      plt.xlabel('Day')
-      plt.ylabel('Value')
-      plt.legend()
-      plt.grid(True)
-      plt.show()
+      # wykres 100 dni
 
+      buy_points, sell_points, intersection_points = analyze_macd(macd_values, signal_values, dataset_display, 10)
 
-
-
-      # drugi wykres
-
-      plt.figure(figsize=(10, 6))
-      plt.plot(dataset_display.index[:500], macd_values[:500], label='MACD', color='blue')
-      plt.plot(dataset_display.index[:500], signal_values[:500], label='Signal Line', color='red')
-
-
-      buy_points = []
-      sell_points = []
-      intersection_points = []
-      for i in range(1, len(macd_values)//2):
-            if macd_values[i] > signal_values[i] and macd_values[i - 1] <= signal_values[i - 1]:
-                  buy_points.append((dataset_display.index[i], dataset_display["close"].iloc[i]))
-                  intersection_points.append((dataset_display.index[i], macd_values[i], BUY))
-            elif macd_values[i] < signal_values[i] and macd_values[i - 1] >= signal_values[i - 1]:
-                  sell_points.append((dataset_display.index[i], dataset_display["close"].iloc[i]))
-                  intersection_points.append((dataset_display.index[i], macd_values[i], SELL))
-
-
-
-      for point in buy_points:
-            plt.scatter(point[0], point[1], color='green', marker='^', s=50, zorder=3)
-
-      for point in sell_points:
-            plt.scatter(point[0], point[1], color='black', marker='v', s=50, zorder=3)
-
-      for point, value, _ in intersection_points:
-            plt.scatter(point, value, color='orange', marker='o', s=50, zorder=3)
-
-
-
-
-      plt.plot(dataset_display.index[:500], dataset_display["close"][:500], label='AU', color='yellow', zorder=2)
-      plt.title('MACD and Signal Line')
-      plt.xlabel('Day')
-      plt.ylabel('Value')
-      plt.legend()
-      plt.grid(True)
-      plt.show()
-
-
-
-      # trzeci
-
-      plt.figure(figsize=(10, 6))
-      plt.plot(dataset_display.index[:100], macd_values[:100], label='MACD', color='blue')
-      plt.plot(dataset_display.index[:100], signal_values[:100], label='Signal Line', color='red')
-
-
-      buy_points = []
-      sell_points = []
-      intersection_points = []
-      for i in range(1, len(macd_values)//10):
-            if macd_values[i] > signal_values[i] and macd_values[i - 1] <= signal_values[i - 1]:
-                  buy_points.append((dataset_display.index[i], dataset_display["close"].iloc[i]))
-                  intersection_points.append((dataset_display.index[i], macd_values[i], BUY))
-            elif macd_values[i] < signal_values[i] and macd_values[i - 1] >= signal_values[i - 1]:
-                  sell_points.append((dataset_display.index[i], dataset_display["close"].iloc[i]))
-                  intersection_points.append((dataset_display.index[i], macd_values[i], SELL))
-
-
-
-      for point in buy_points:
-            plt.scatter(point[0], point[1], color='green', marker='^', s=50, zorder=3)
-
-      for point in sell_points:
-            plt.scatter(point[0], point[1], color='black', marker='v', s=50, zorder=3)
-
-      for point, value, _ in intersection_points:
-            plt.scatter(point, value, color='orange', marker='o', s=50, zorder=3)
-
-
-
-
-      plt.plot(dataset_display.index[:100], dataset_display["close"][:100], label='AU', color='yellow', zorder=2)
-      plt.title('MACD and Signal Line')
-      plt.xlabel('Day')
-      plt.ylabel('Value')
-      plt.legend()
-      plt.grid(True)
-      plt.show()
+      display_chart(macd_values, signal_values, dataset_display, buy_points, sell_points, intersection_points, LABEL, 100)
 
    
 
--- macd/methods.py
+++ macd/methods.py
@@ -1,6 +1,62 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+
+
+BUY = 1
+SELL = 0
+INITIAL_BALANCE = 10000
+
+
+
+def display_chart(macd_values: list[float],
+                  signal_values: list[float],
+                  dataset: pd.DataFrame,
+                  buy_points: list,
+                  sell_points: list,
+                  intersection_points: list,
+                  label: str,
+                  rng : int = None) -> None:
+      
+
+      plt.figure(figsize=(10, 6))
+      plt.plot(dataset.index[:rng], macd_values[:rng], label='MACD', color='blue')
+      plt.plot(dataset.index[:rng], signal_values[:rng], label='Signal Line', color='red')
+
+      for point in buy_points:
+            plt.scatter(point[0], point[1], color='green', marker='^', s=50, zorder=3)
+
+      for point in sell_points:
+            plt.scatter(point[0], point[1], color='black', marker='v', s=50, zorder=3)
+
+      for point, value, _ in intersection_points:
+            plt.scatter(point, value, color='orange', marker='o', s=50, zorder=3)
+
+
+      plt.plot(dataset.index[:rng], dataset["close"][:rng], label=label, color='yellow', zorder=2)
+      plt.title('MACD and Signal Line')
+      plt.xlabel('Day')
+      plt.ylabel('Value')
+      plt.legend()
+      plt.grid(True)
+      plt.show()
+
+
+def analyze_macd(macd_values : list[float], signal_values : list[float], dataset : pd.DataFrame, divisor : int = 1 ):
+      buy_points = []
+      sell_points = []
+      intersection_points = []
+      for i in range(1, len(macd_values)//divisor):
+            if macd_values[i] > signal_values[i] and macd_values[i - 1] <= signal_values[i - 1]:
+                  buy_points.append((dataset.index[i], dataset["close"].iloc[i]))
+                  intersection_points.append((dataset.index[i], macd_values[i], BUY))
+            elif macd_values[i] < signal_values[i] and macd_values[i - 1] >= signal_values[i - 1]:
+                  sell_points.append((dataset.index[i], dataset["close"].iloc[i]))
+                  intersection_points.append((dataset.index[i], macd_values[i], SELL))
+
+
+      return buy_points, sell_points, intersection_points
 
 
 def ema(periods : int, dataset : pd.DataFrame, start: int) -> float:
@@ -38,3 +94,80 @@ def macd(dataset : pd.DataFrame) -> list[float]:
             macd_values.append(ema(12, dataset, day) - ema(26, dataset, day))
 
       return macd_values
+
+
+
+INITIAL_BALANCE = 0
+INITIAL_COINS = 1000
+RSI_PERIOD = 14
+BUY = 1
+SELL = 0
+
+
+
+
+
+def calculate_rsi(prices):
+    delta = prices.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=RSI_PERIOD).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=RSI_PERIOD).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def simulate_investment_rsi(intersection_points: list, dataset_display: pd.DataFrame) -> float:
+      balance = 0
+      coins = INITIAL_COINS
+      price = dataset_display["close"][26]
+      print(f"Starting with rsi with balance: {coins * price}")
+
+      rsi_values = calculate_rsi(dataset_display["close"])
+
+      for point in intersection_points:
+            index, _, state = point
+            price = dataset_display["close"][index]
+            rsi = rsi_values[index]
+
+            if state == BUY and rsi < 30:  # oversold
+                  coins_to_buy = balance / price
+                  balance -= coins_to_buy * price
+                  coins += coins_to_buy
+                  #print(f"Buying {coins_to_buy:.8f} coins at ${price:.2f} each. Balance: ${balance:.2f}")
+            elif state == SELL and rsi > 70:  # overbought
+                  balance += coins * price
+                  #print(f"Selling {coins:.8f} coins at ${price:.2f} each. Balance: ${balance:.2f}")
+                  coins = 0
+
+      if coins > 0:
+            balance += coins * dataset_display["close"].iloc[-1]
+            #print(f"Selling remaining {coins:.8f} coins at ${dataset_display['close'].iloc[-1]:.2f} each. Balance: ${balance:.2f}")
+
+      return balance
+
+
+def simulate_investment(intersection_points: list, dataset_display: pd.DataFrame) -> float:
+      balance = 0
+      coins = INITIAL_COINS
+      price = dataset_display["close"][26]
+      print(f"Starting simple with balance: {coins * price}")
+
+      for point in intersection_points:
+            index, _, state = point
+            price = dataset_display["close"][index]
+
+            if state == BUY:
+                  coins_to_buy = balance / price
+                  balance -= coins_to_buy * price
+                  coins += coins_to_buy
+                  print(f"Buying {coins_to_buy:.8f} coins at ${price:.2f} each. Balance: ${balance:.2f}")
+            elif state == SELL:
+                  balance += coins * price
+                  print(f"Selling {coins:.8f} coins at ${price:.2f} each. Balance: ${balance:.2f}")
+                  coins = 0
+
+      if coins > 0:
+            balance += coins * dataset_display["close"].iloc[-1]
+            print(f"Selling remaining {coins:.8f} coins at ${dataset_display['close'].iloc[-1]:.2f} each. Balance: ${balance:.2f}")
+
+      return balance
+
